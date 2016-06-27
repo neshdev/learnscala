7111ddde93d3acc74628042bea094f0b07adafe8
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2016-06-27 11:04:05 -0400

    finished assignment 1

--- recfun/recfun/src/main/scala/recfun/Main.scala
+++ recfun/recfun/src/main/scala/recfun/Main.scala
@@ -34,5 +34,9 @@ object Main {
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if ( money < 0 || coins.isEmpty) 0
+    else if ( money == 0)  1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
